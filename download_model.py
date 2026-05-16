b881e5b8515c451230d93ad78675518cd2cf8d6f
Satheesh <sathish.naga33@gmail.com>
2026-05-16 13:05:56 +0530

    add google drive link for model download

--- download_model.py
+++ download_model.py
@@ -5,7 +5,7 @@ MODEL_PATH = "resume_model.pth"
 
 FILE_ID = "YOUR_FILE_ID"
 
-URL = f"https://drive.google.com/uc?id={FILE_ID}"
+URL = f"https://drive.google.com/file/d/1P0mqh3pJLy6-xEGGnzb_8xfoF5reYa3k/view?usp=sharing"
 
 if not os.path.exists(MODEL_PATH):
     print("Downloading model...")
